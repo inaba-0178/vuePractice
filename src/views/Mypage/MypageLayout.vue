b972c64d9dc2fc4f3fb45c5847dc0d6643729bc7
inaba-0178 <76714403+inaba-0178@users.noreply.github.com>
2026-03-12 17:33:27 +0900

    パスワード変更追加

--- src/views/Mypage/MypageLayout.vue
+++ src/views/Mypage/MypageLayout.vue
@@ -9,6 +9,12 @@
           </svg>
           登録情報
         </router-link>
+        <router-link to="/mypage/email" class="sidebar__item" :class="{ active: $route.path === '/mypage' || $route.path === '/mypage/email' }">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+          </svg>
+          メールアドレス変更
+        </router-link>
       </nav>
       <button @click="handleLogout" class="sidebar__logout">ログアウト</button>
     </aside>
